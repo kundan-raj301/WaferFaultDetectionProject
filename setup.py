3d06352f0c304ae61163fc810b8ca73efa88467c
Kundan Kumar <rajkundan301@gmail.com>
2023-09-03 13:39:12 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="WaferfaultDetection",
+    version="0.0.1",
+    author="Kundan",
+    author_email="rajkundan301@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
